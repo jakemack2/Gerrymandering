ea1a6971e6ea94da97ce81796c6cd3995cb26f27
jakemack2 <78124901+jakemack2@users.noreply.github.com>
2021-01-27 22:41:29 -0500

    Update Democrat.java

--- Democrat.java
+++ Democrat.java
@@ -1,6 +1,16 @@
 
 public class Democrat extends Voter {
 
+	public static int getDemocrat
+	{
+		
+	}
+	
+	public String toString
+	{
+		return "Total number of Democrats" + nD;
+		
+	}
 	// return appropriate constants when implementing getAffiliation
 	// toString method
 }
