835df00f1733ad90d9ee3bb67e8150153a574836
jakemack2 <78124901+jakemack2@users.noreply.github.com>
2021-01-27 21:28:26 -0500

    Update Voter.java

--- Voter.java
+++ Voter.java
@@ -6,10 +6,10 @@
 public class Voter {
 
 	// need two public constants democrat and republican
-			public static final int DEMOCRAT = 0;
-			public static final int REPUBLICAN = 1;
+	public static final int DEMOCRAT = 0;
+	public static final int REPUBLICAN = 1;
 			
-			// abstract method int getAffiliation
+	// abstract method int getAffiliation
 	abstract public void getAffiliation;
 			
 	public static void main(String[] args) {
