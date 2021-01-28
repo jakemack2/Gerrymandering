77e4ff80f90f55120cfd57d3ea481d6889ea5434
jakemack2 <78124901+jakemack2@users.noreply.github.com>
2021-01-27 22:43:00 -0500

    Update Republican.java

--- Republican.java
+++ Republican.java
@@ -1,5 +1,15 @@
 
 public class Republican extends Voter {
+	
+	public static int getRepublican
+	{
+		
+	}
+	
+	public String toString
+	{
+		return "Total Number of Republicans" + nR;
+	}
 
 	// return appropriate constants when implementing getAffiliation
 	// toString method
